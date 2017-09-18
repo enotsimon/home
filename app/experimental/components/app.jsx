ef357a2841571314bddf1ce4eff793c06357c0f0
enotsimon <sy.enot@gmail.com>
2017-09-18 09:11:31 +0300

    debug additional fields draft

--- app/experimental/components/app.jsx
+++ app/experimental/components/app.jsx
@@ -3,6 +3,11 @@ import DebugInfo from 'experimental/components/debug_info';
 import React from 'react';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {additional: props.additional ? props.additional : []};
+  }
+
   render() {
     return (
       <div className="panel-group">
@@ -20,6 +25,7 @@ export default class App extends React.Component {
             <div>
               <div>FPS: <span id="fps_counter"></span></div>
               <div>mouse position: <span id="mouse_pos">{0, 0}</span></div>
+              {this.state.additional.forEach(e => <div>{e.text}: <span id={e.id}>{e.default}</span></div>)}
             </div>
           </div>
         </div>
