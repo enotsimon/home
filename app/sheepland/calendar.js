3a9d3d0df703bdc419a32b07cd6c3bcf8877f0a4
Иван Скоробогатько <skorobogatko@superjob.ru>
2017-10-05 15:15:44 +0300

    make day 1 min long

--- app/sheepland/calendar.js
+++ app/sheepland/calendar.js
@@ -4,7 +4,8 @@ import {game} from "sheepland/sheepland";
 
 export default class Calendar {
   constructor() {
-    this.ticks_per_day = 1000/game.tick_basic_delay*60*5; // day lasts 5 min
+    //this.ticks_per_day = 1000/game.tick_basic_delay*60*5; // day lasts 5 min
+    this.ticks_per_day = 1000/game.tick_basic_delay*5; // day lasts 1 min
     this.basic_time_ratio = 1000*60*60*24/this.ticks_per_day;
 
     this.date = new Date(-1346, 10, 4, 12, 5, 1, 0); // just for fun
