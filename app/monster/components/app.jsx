be657b8961e8c0fdcaec4e84a96ddc39761d4c0f
enot simon <sy.enot@gmail.com>
2017-12-01 15:51:29 +0300

    main_menu_element activity (dont work)

--- app/monster/components/app.jsx
+++ app/monster/components/app.jsx
@@ -11,8 +11,8 @@ export default class App extends React.Component {
     // TEMPORARY FAKE DATA
     let main_menu_data = {
       elements: [
-        {id: 'go_to', text: 'go to ...'},
-        {id: 'speak_to', text: 'speak to ...'},
+        {id: 'go_to', text: 'go to ...', active: false},
+        {id: 'speak_to', text: 'speak to ...', active: false},
       ],
       on_element_click: (id) => console.log('dont believe it works! click on', id),
     };
