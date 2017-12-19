a997535e041e7502d4c75d1200e778e6d69577dc
Иван Скоробогатько <skorobogatko@superjob.ru>
2017-12-19 08:16:54 +0300

    add scene changing from main menu (test code)

--- app/monster/components/main_menu_container.jsx
+++ app/monster/components/main_menu_container.jsx
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux'
 import MainMenu from './main_menu';
 import {main_menu_click, main_menu_subelement_click} from 'monster/actions';
+import game from 'monster/monster';
 
 
 const mapStateToProps = state => {
@@ -15,6 +16,11 @@ const mapDispatchToProps = dispatch => {
       dispatch(main_menu_click(id));
     },
     on_subelement_click: id => {
+      // very bad place for that, just a test code
+      let state = game.store.getState(); // shit!
+      if (state.menues.main_menu.current_element == 'go_to') {
+        game.change_scene(id);
+      }
       dispatch(main_menu_subelement_click(id));
     },
   };
