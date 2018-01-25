e9735aa9018490100e12c7f6189ee5d527f94aec
enotsimon <sy.enot@gmail.com>
2018-01-25 23:52:25 +0300

    add simple items list to container view. as plain text for now

--- app/monster/components/inspect_furniture.jsx
+++ app/monster/components/inspect_furniture.jsx
@@ -1,30 +1,30 @@
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import TextEntry from './text_entry';
+import React from 'react'
+import PropTypes from 'prop-types'
+import TextEntry from './text_entry'
+import Panel from 'common/components/panel'
 
 const InspectFurniture = (props) => {
   return (
-    <div className="panel panel-success">
-      <div className="panel-heading">
-        <h4 className="panel-title">
-          <TextEntry>
-            {props.furniture_name}
-          </TextEntry>
-        </h4>
-      </div>
-      <div className="panel-body">
-        <TextEntry>
-          {props.description}
-        </TextEntry>
-      </div>
-    </div>
+    <Panel header={props.furniture_name}>
+      <TextEntry>
+        {props.description}
+      </TextEntry>
+
+      <div className="spacer"></div>
+
+      <Panel header={props.items_list_text}>
+        {props.items_list.map(item => <TextEntry key={item}>{item}</TextEntry>)}
+      </Panel>
+    </Panel>
   );
 }
 
 InspectFurniture.propTypes = {
   furniture_name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  items_list_text: PropTypes.string.isRequired,
+  items_list: PropTypes.array.isRequired,
 };
 
 export default InspectFurniture;
--- app/monster/components/inspect_furniture_container.jsx
+++ app/monster/components/inspect_furniture_container.jsx
@@ -7,9 +7,22 @@ const state_to_props = state => {
   if (!text) {
     throw({msg: "furniture text not found in config", id_furniture: state.inspect.id_furniture});
   }
+  let container = state.containers[state.inspect.id_furniture];
+  if (!container) {
+    throw({msg: "container not found by id", id_furniture: state.inspect.id_furniture});
+  }
   return {
     furniture_name: text.name,
     description: text.description,
+    items_list_text: game.config.text.menues.inspect_furniture.items_list,
+    items_list: container.items.map(id_item => {
+      let item = state.items[id_item]
+      let text = game.config.text.items[item.type]
+      if (!text) {
+        throw({msg: "item text not found in config", id_item})
+      }
+      return text
+    })
   };
 }
 
