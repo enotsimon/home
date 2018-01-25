fbd1b8df60e35fae72ea15195bad9e81dadd84bd
enotsimon <sy.enot@gmail.com>
2018-01-26 01:35:46 +0300

    rewrite Dialog using SimplePanelSuccess

--- app/monster/components/dialog.jsx
+++ app/monster/components/dialog.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TextEntry from './text_entry';
 import StButton from './st_button';
+import {SimplePanelSuccess} from 'common/components/panel'
 
 class Dialog extends React.Component {
   mobile_name(name) {
@@ -10,33 +11,28 @@ class Dialog extends React.Component {
 
   render() {
     return (
-      <div className="panel panel-success">
-        <div className="panel-heading">
-          <h4 className="panel-title">
-            dialog
-          </h4>
-        </div>
-        <div className="panel-body">
-          {/* TODO add player name so we can understand who's speaking */}
-          <p><TextEntry>{this.props.player_prev_sentence.phrases}</TextEntry></p>
+      <SimplePanelSuccess title="dialog">
+          
+        {/* TODO add player name so we can understand who's speaking */}
+        <p><TextEntry>{this.props.player_prev_sentence.phrases}</TextEntry></p>
 
-          <p>
-            {this.mobile_name(this.props.npc_name)}
-            <TextEntry>{this.props.npc_sentence.phrases}</TextEntry>
-          </p>
+        <p>
+          {this.mobile_name(this.props.npc_name)}
+          <TextEntry>{this.props.npc_sentence.phrases}</TextEntry>
+        </p>
 
-          {/* TODO add player name so we can understand who's speaking */}
-          {this.props.player_sentences.map(sentence => {
-            return (
-              <StButton key={sentence.id} on_click={() => this.props.on_player_sentence_click(sentence.id)}>
-                <TextEntry>
-                  {sentence.phrases}
-                </TextEntry>
-              </StButton>
-            );
-          })}
-        </div>
-      </div>
+        {/* TODO add player name so we can understand who's speaking */}
+        {this.props.player_sentences.map(sentence => {
+          return (
+            <StButton key={sentence.id} on_click={() => this.props.on_player_sentence_click(sentence.id)}>
+              <TextEntry>
+                {sentence.phrases}
+              </TextEntry>
+            </StButton>
+          );
+        })}
+        
+      </SimplePanelSuccess>
     );
   }
 }
