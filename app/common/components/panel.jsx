9117b895f3d5c3a239a54d1afca9fda8618b5eab
enotsimon <sy.enot@gmail.com>
2018-01-26 00:44:22 +0300

    add SimplePanelSuccess and rewrite InspectFurniture using it

--- app/common/components/panel.jsx
+++ app/common/components/panel.jsx
@@ -1,12 +1,55 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export const PanelSuccess = (props) => {
+const Panel = (props) => {
   return (
-    <div className="panel panel-success">
+    <div className={"panel panel-" + props.type}>
       {props.children}
     </div>
   );
 }
+Panel.propTypes = {
+  type: PropTypes.oneOf(['success']).isRequired,
+}
+export {Panel}
+
+const SimplePanel = (props) => {
+  return (
+    <Panel type={props.type}>
+      <PanelTitle>
+        {props.title}
+      </PanelTitle>
+      <PanelBody>
+        {props.children}
+      </PanelBody>
+    </Panel>
+  );
+}
+SimplePanel.propTypes = {
+  type: PropTypes.oneOf(['success']).isRequired,
+  title: PropTypes.string.isRequired,
+}
+export {SimplePanel}
+
+export const PanelSuccess = (props) => {
+  return (
+    <Panel type="success">
+      {props.children}
+    </Panel>
+  );
+}
+
+const SimplePanelSuccess = (props) => {
+  return (
+    <SimplePanel type="success" title={props.title}>
+      {props.children}
+    </SimplePanel>
+  );
+}
+SimplePanelSuccess.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+export {SimplePanelSuccess}
 
 export const PanelTitle = (props) => {
   return (
