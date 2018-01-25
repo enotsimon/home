56f9f613b3d796c4272e2382452be5ed104553cd
enotsimon <sy.enot@gmail.com>
2018-01-26 01:40:16 +0300

    rewrite st_button.jsx a bit

--- app/monster/components/st_button.jsx
+++ app/monster/components/st_button.jsx
@@ -2,28 +2,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class StButton extends React.Component {
-  btn_class() {
-    let btn_type = !this.props.active ? 'btn-default' : 'btn-success';
-    return `btn ${btn_type} btn-block`;
-  }
+const btn_class = (active) => {
+  let btn_type = !active ? 'btn-default' : 'btn-success';
+  return `btn ${btn_type} btn-block`;
+}
 
-  render() {
-    return (
-      <button
-        //id={this.props.id}
-        type="button"
-        className={this.btn_class()}
-        onClick={this.props.on_click}
-        // by default button is enabled
-        disabled={this.props.enabled === false ? 'disabled' : ''}
-      >
-        <div className='text-left'>
-          {this.props.children}
-        </div>
-      </button>
-    );
-  }
+const StButton = ({on_click: on_click, active: active = false, enabled: enabled = true, children: children}) => {
+  return (
+    <button
+      type="button"
+      className={btn_class(active)}
+      onClick={on_click}
+      // by default button is enabled
+      disabled={enabled === false ? 'disabled' : ''}
+    >
+      <div className='text-left'>
+        {children}
+      </div>
+    </button>
+  );
 }
 
 StButton.propTypes = {
