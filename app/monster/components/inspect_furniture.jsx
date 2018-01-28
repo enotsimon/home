cc10a2d67bbef3254cee777fbf376e164f52a6ce
enotsimon <sy.enot@gmail.com>
2018-01-28 19:32:11 +0300

    add 'pick up' and 'inspect' buttons stubs in container menu

--- app/monster/components/inspect_furniture.jsx
+++ app/monster/components/inspect_furniture.jsx
@@ -5,10 +5,6 @@ import TextEntry from './text_entry'
 import {SimplePanelSuccess} from 'common/components/panel'
 import StButton from './st_button'
 
-const on_item_click = (id_item) => {
-  console.log('on_item_click', id_item);
-}
-
 const InspectFurniture = (props) => {
   return (
     <SimplePanelSuccess title={props.furniture_name}>
@@ -19,21 +15,44 @@ const InspectFurniture = (props) => {
       <div className="spacer"></div>
 
       <SimplePanelSuccess title={props.items_list_text}>
-        {props.items_list.map(id_item =>
-          <StButton key={id_item} on_click={on_item_click} block={false}>
-            {id_item}
-          </StButton>
-        )}
+        {props.items_list.map(e => {
+          let active = e.id_item === props.active_item
+          return (<StButton key={e.id_item} on_click={() => props.on_item_click(e.id_item)} block={false} active={active}>
+            {e.text}
+          </StButton>)
+        })}
+        {props.active_item &&
+          <div>
+            <div className='spacer'></div>
+            <StButton on_click={() => props.on_pick_up_item_click(props.active_item)} block={false}>
+              {props.pick_up_text}
+            </StButton>
+            <StButton on_click={() => props.on_inspect_item_click(props.active_item)} block={false}>
+              {props.inspect_text}
+            </StButton>
+          </div>
+        }
       </SimplePanelSuccess>
     </SimplePanelSuccess>
   );
 }
 
 InspectFurniture.propTypes = {
+  items_list_text: PropTypes.string.isRequired,
+  pick_up_text: PropTypes.string.isRequired,
+  inspect_text: PropTypes.string.isRequired,
   furniture_name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  items_list_text: PropTypes.string.isRequired,
-  items_list: PropTypes.array.isRequired,
+  items_list: PropTypes.arrayOf(
+    PropTypes.shape({
+      id_item: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  active_item: PropTypes.string, // string or null
+  on_item_click: PropTypes.func.isRequired,
+  on_pick_up_item_click: PropTypes.func.isRequired,
+  on_inspect_item_click: PropTypes.func.isRequired,
 };
 
 export default InspectFurniture;
