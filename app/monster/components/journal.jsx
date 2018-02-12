0ffcca2c9d5a569a9459c1384ff70875f7f52b39
enotsimon <sy.enot@gmail.com>
2018-02-12 22:43:48 +0300

    some new features in journal

--- app/monster/components/journal.jsx
+++ app/monster/components/journal.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 
 import TextEntry from './text_entry'
 import {SimplePanelSuccess} from 'common/components/panel'
 import StButton from './st_button'
 
-const Journal = (props) => {
-  return (
-    <SimplePanelSuccess title={props.title}>
-      <pre>
-        {props.data.map((e, i) => (
-          <div key={i}>
-            <span style={{color: e.color, fontWeight: 'bold'}}>[{e.level}]</span>&nbsp;
-            <TextEntry>{e.msg}</TextEntry>
-          </div>
-        ))}
-      </pre>
-    </SimplePanelSuccess>
-  );
+class Journal extends React.Component {
+  componentDidMount() {
+    this.scroll_to_bottom()
+  }
+
+  componentDidUpdate() {
+    this.scroll_to_bottom()
+  }
+
+  scroll_to_bottom() {
+    let journal_container = ReactDOM.findDOMNode(this.journal_container)
+    journal_container.scrollTop = journal_container.scrollHeight
+  }
+
+  render() {
+    return (
+      <SimplePanelSuccess title={this.props.title}>
+        <div className='journal-container' ref={e => this.journal_container = e}>
+          {this.props.data.map((e, i) => (
+            <div key={i}>
+              <span style={{color: e.color, fontWeight: 'bold'}}>[{e.level}]</span>&nbsp;
+              <TextEntry>{e.msg}</TextEntry>
+            </div>
+          ))}
+        </div>
+      </SimplePanelSuccess>
+    )
+  }
 }
 
 Journal.propTypes = {
