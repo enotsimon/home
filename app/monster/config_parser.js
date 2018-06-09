86c3dc4c74a18ee7b974715eef80c83694130919
enotsimon <sy.enot@gmail.com>
2018-06-09 17:11:16 +0300

    rewrite config_parser

--- app/monster/config_parser.js
+++ app/monster/config_parser.js
@@ -5,68 +5,98 @@ import yaml from 'js-yaml'
 export const parse_raw = (yaml_doc) => yaml.safeLoad(yaml_doc)
 export const parse_dialogs = (yaml_doc) => parse_yaml_config_dialogs(parse_raw(yaml_doc))
 
-// i hate it
-let dialogs_conf = []
-let i = 0
-
 const parse_yaml_config_dialogs = (dialogs) => {
-  dialogs = parse_dialog_process_config_tree(parse_dialog_set_id, dialogs)
-  dialogs = parse_dialog_process_config_tree(parse_dialog_expand_macro, dialogs)
-  dialogs_conf = []
-  parse_dialog_process_config_tree(parse_dialog_process_element, dialogs)
-  let conf_as_object = R.reduce((acc, e) => ({...acc, [e.id]: e}), {}, dialogs_conf)
-  console.log('conf_as_object', conf_as_object)
-  return conf_as_object
+  // map is because in dialogs config organized as array, and next element in this array
+  // is NOT logical next element for it
+  let keys = ['sequence', 'seq', 'choose']
+  dialogs.forEach(e => walk_tree(e, null, null, keys, set_id, [e.id, 1]))
+  dialogs.forEach(e => walk_tree(e, null, null, keys, set_parent_and_next))
+  let conf = R.mergeAll(R.map(e => walk_tree(e, null, null, keys, flatten_tree, {}), dialogs))
+  conf = R.map(e => replace_arrays_of_elements_with_arrays_of_ids(e, keys), conf)
+  // not shure if we should do it here, maybe before flatten_tree
+  conf = R.map(expand_macro, conf)
+  conf = R.map(seq_to_sequence, conf)
+  conf = R.map(process_element, conf)
+  
+  console.log('conf', conf)
+  return conf
 }
 
-// TODO not sure if its good idea to pass parent_element, next_element
-// cause it makes this func very specific
-const parse_dialog_process_config_tree = (func, config, next_element = null, parent_element = null) => {
-  if (config instanceof Array) {
-    //return config.map(e => parse_dialog_process_config_tree(func, e, next_element, ))
-    let pairs = R.aperture(2, [...config, null])
-    return pairs.map(([e, next]) => parse_dialog_process_config_tree(func, e, next, parent_element))
+/**
+ * this func walks tree recursively applying func to every element and checking props
+ * from keys as 'points of tree branching' func should be like
+ * (element, parent_element, next_element, acc) -> acc
+ */
+const walk_tree = (element, parent, next, keys, func, acc = []) => {
+  if (element instanceof Array) {
+    let pairs = R.aperture(2, [...element, null])
+    return pairs.reduce((acc, [e1, e2]) => walk_tree(e1, parent, e2, keys, func, acc), acc, pairs)
   } else {
-    let element = func(config, next_element, parent_element)
-    if (element.sequence && element.choose) {
-      throw({msg: "dialogs config element got both 'sequence' and 'choose' props", element})
+    acc = func(element, parent, next, acc)
+    let filtered_keys = keys.filter(key => element[key])
+    if (filtered_keys.length > 1) {
+      throw({msg: 'element has more than one branching key', keys, element})
     }
-    if (element.sequence) {
-      element.sequence = parse_dialog_process_config_tree(func, element.sequence, null, element)
+    if (filtered_keys.length === 1) {
+      acc = walk_tree(element[filtered_keys[0]], element, null, keys, func, acc)
     }
-    if (element.choose) {
-      element.choose = parse_dialog_process_config_tree(func, element.choose, null, element)
-    }
-    return element
+    return acc
   }
 }
 
 // TODO add id prefix
-const parse_dialog_set_id = (element) => {
-  return {...element, id: element.id || '' + ++i}
+const set_id = (element, _parent, _next, [prefix, i]) => {
+  element.id = element.id || prefix + '.' + ++i
+  return [prefix, i]
+}
+
+const set_parent_and_next = (element, parent, next) => {
+  element.parent = parent
+  element.next = next
+}
+
+const flatten_tree = (element, _parent, _next, acc) => {
+  return {...acc, [element.id]: element}
+}
+
+const replace_arrays_of_elements_with_arrays_of_ids = (element, keys) => {
+  let clone = {...element}
+  keys.forEach(key => {
+    if (clone[key]) {
+      clone[key] = clone[key].map(e => e.id)
+    }
+  })
+  return clone
 }
 
 // TODO
-const parse_dialog_expand_macro = (element, next_element, parent_element) => {
+const expand_macro = element => {
   return element
 }
 
-const parse_dialog_process_element = (element, next_element, parent_element) => {
-  let cell = {
+const seq_to_sequence = element => {
+  if (element.seq) {
+    let {seq, ...clone} = element
+    return {...clone, sequence: element.seq}
+  }
+  return element
+}
+
+const process_element = element => {
+  let ret = {
     id: element.id,
-    cond: parse_dialog_cond(element),
+    cond: parse_cond(element),
     car: get_cell_car(element),
-    cdr: get_cell_cdr(element, next_element, parent_element),
+    cdr: get_cell_cdr(element),
     before: element.before, // TODO parse
     after: element.after, // TODO parse
   }
-  // its a damn crap! rewrite it ASAP!
-  dialogs_conf = [...dialogs_conf, cell]
-  return element // yes! we need it for parse_dialog_process_config_tree()
+  //console.log('ret', ret)
+  return ret
 }
 
 // TODO add parsing of data
-const parse_dialog_cond = element => {
+const parse_cond = element => {
   if (element.cond && element.if) {
     throw({msg: "element got both 'cond' and 'if' props", element})
   }
@@ -79,7 +109,7 @@ const get_cell_car = element => {
     return {...phrases, type: 'phrase'}
   } else if (element.sequence) {
     // {type: 'link', id: element.sequence[0].id} ???
-    return element.sequence[0].id
+    return element.sequence[0]
   } else if (element.choose) {
     // crap!
     return {type: 'choose', ids: element.choose.map(e => e.id)}
@@ -88,24 +118,24 @@ const get_cell_car = element => {
   }
 }
 
-const get_cell_cdr = (element, next_element, parent_element) => {
+const get_cell_cdr = (element) => {
   // parent_element.car.type !!! ??? this may change
-  if (parent_element && parent_element.car && parent_element.car.type === 'choose') {
+  if (element.parent && element.parent.car && element.parent.car.type === 'choose') {
     return null
   } else if (element.goto) {
-    // should it be here or in parse_dialog_expand_macro()?
+    // should it be here or in expand_macro()?
     return element.goto
-  } else if (next_element === null) {
+  } else if (element.next === null) {
     return null
   } else if (element.sequence) {
     return null
   } else if (element.choose) {
     return null
   } else {
-    if (!next_element.id) {
+    if (!element.next.id) {
       throw({msg: 'cannot set cell cdr cause next_element has no id', element, next_element})
     }
-    return next_element.id
+    return element.next.id
   }
 }
 
