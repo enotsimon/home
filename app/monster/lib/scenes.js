7d51cf360d9b1dc97fc3d56d37a70695c7745ef5
enot simon <sy.enot@gmail.com>
2018-06-12 12:56:08 +0300

    fix scenes.js import

--- app/monster/lib/scenes.js
+++ app/monster/lib/scenes.js
@@ -1,5 +1,5 @@
 
-import {check_conds_list} from './preconditions'
+import {check_conds_list} from './cond'
 
 export const get_scene_available_links = (scene) => {
   return scene.links.filter(e => check_conds_list(e.cond)).map(link => link.id_scene)
