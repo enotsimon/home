280dfdbb3d43376fc9de1e910f1779b315bd5855
enot simon <sy.enot@gmail.com>
2018-06-12 12:23:29 +0300

    rename preconditions.js to cond.js and add some new stuff there

--- app/monster/lib/scenes.js
+++ app/monster/lib/scenes.js
@@ -1,7 +1,6 @@
-import game from '../monster'
-import {check_preconditions} from './preconditions'
-import {apply_consequences} from './consequences'
+
+import {check_conds_list} from './preconditions'
 
 export const get_scene_available_links = (scene) => {
-  return scene.links.filter(check_preconditions).map(link => link.id_scene)
+  return scene.links.filter(e => check_conds_list(e.cond)).map(link => link.id_scene)
 }
