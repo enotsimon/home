8ce74f791007015ece1f336fe5bddb9e6e421682
enotsimon <sy.enot@gmail.com>
2018-06-21 19:15:49 +0300

    rewrite dialog.jsx using flow

--- app/monster/components/dialog.jsx
+++ app/monster/components/dialog.jsx
@@ -1,53 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import TextEntry from './text_entry';
-import StButton from './st_button';
+import React from 'react'
+import TextEntry from './text_entry'
+import StButton from './st_button'
 import {SimplePanelSuccess} from 'common/components/panel'
 
-class Dialog extends React.Component {
-  mobile_name(name) {
-    return name ? (<span><TextEntry>{name}</TextEntry>:&nbsp;</span>) : '';
-  }
+import type {action_dialog_phrase, id_dialog_cell} from './types/dialog_types'
 
-  render() {
-    return (
-      <SimplePanelSuccess title="dialog">
-
-        {this.props.phrases.map(phrase =>
-          <div key={phrase.id}>
-            <span><TextEntry>{phrase.owner}</TextEntry>:&nbsp;</span>
-            <TextEntry>{phrase.phrases}</TextEntry>
-          </div>
-        )}
-
-        {/* TODO add player name so we can understand who's speaking */}
-        {this.props.player_sentences.map(sentence => {
-          return (
-            <StButton key={sentence.id} on_click={() => this.props.on_player_sentence_click(sentence.id)}>
-              <TextEntry>
-                {sentence.phrases}
-              </TextEntry>
-            </StButton>
-          );
-        })}
-        
-      </SimplePanelSuccess>
-    );
-  }
+type DialogProps = {
+  player_sentences: Array<action_dialog_phrase>,
+  on_player_sentence_click: (id_cell: id_dialog_cell) => void,
+  phrases: Array<action_dialog_phrase>,
 }
 
-Dialog.propTypes = {
-  player_sentences: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    owner: PropTypes.any.isRequired,
-    phrases: PropTypes.any.isRequired,
-  })).isRequired,
-  on_player_sentence_click: PropTypes.func.isRequired,
-  phrases: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    owner: PropTypes.any.isRequired, // ???
-    phrases: PropTypes.any.isRequired,
-  })).isRequired,
-};
+export default function Dialog(props: DialogProps) {
+  return (
+    <SimplePanelSuccess title="dialog">
 
-export default Dialog
+      {props.phrases.map(phrase =>
+        <div key={phrase.id}>
+          <span><TextEntry>{phrase.owner}</TextEntry>:&nbsp;</span>
+          <TextEntry>{phrase.phrases}</TextEntry>
+        </div>
+      )}
+
+      {/* TODO add player name so we can understand who's speaking */}
+      {props.player_sentences.map(sentence => {
+        return (
+          <StButton key={sentence.id} on_click={() => props.on_player_sentence_click(sentence.id)}>
+            <TextEntry>
+              {sentence.phrases}
+            </TextEntry>
+          </StButton>
+        );
+      })}
+      
+    </SimplePanelSuccess>
+  )
+}
