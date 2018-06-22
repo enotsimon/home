1ff15b3e09c2b70479d0720e97b0e646a73dc0e3
enotsimon <sy.enot@gmail.com>
2018-06-22 12:24:54 +0300

    delete all monster code cause it moved to another repo

--- app/monster/types/cond_types.js
+++
@@ -1,8 +0,0 @@
-// @flow
-
-export type cond = {
-  type: "flag",
-  name: string,
-  what?: '==' | '!=' | '>=' | '<=' | '<' | '>', // @TODO remove ? set '==' by defaulf in the parses
-  value: any, // actually string | numbler
-}
--- app/monster/types/flag.js
+++
@@ -1,4 +0,0 @@
-// @flow
-export type flag_name = string
-
-export type flag_value = any
