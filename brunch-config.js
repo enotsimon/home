772d621144ef4cef46aa9bf184289aa0f4543f12
enot simon <sy.enot@gmail.com>
2019-10-26 12:46:15 +0300

    changes in brunch-config.js

--- brunch-config.js
+++ brunch-config.js
@@ -6,23 +6,24 @@ exports.files = {
       'app.js': /^app/
     }
   },
-  stylesheets: {joinTo: 'app.css'}
-};
+  stylesheets: { joinTo: 'app.css' }
+}
 
 exports.plugins = {
   babel: {
     presets: ['latest', 'react'],
-    plugins: ["transform-object-rest-spread", "transform-flow-strip-types"]
+    // plugins: ["transform-object-rest-spread", "transform-flow-strip-types"]
+    plugins: ['transform-object-rest-spread']
   },
-  flowtype: {
-    warnOnly: false,
-    method: "status", // check-contents
-    statusDelay: 250
-  }
-};
+  // flowtype: {
+  //   warnOnly: false,
+  //   method: "status", // check-contents
+  //   statusDelay: 250
+  // }
+}
 
 exports.server = {
   port: 3334
-  //base: '/myapp',
-  //stripSlashes: true
-};
+  // base: '/myapp',
+  // stripSlashes: true
+}
