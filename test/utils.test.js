df1a55dd99f4b658b294c2acb75188914bd86b93
enot simon <sy.enot@gmail.com>
2019-10-26 12:10:00 +0300

    add first fake test

---
+++ test/utils.test.js
@@ -0,0 +1,10 @@
+// @flow
+/* eslint-env mocha */
+import { describe, it } from 'mocha'
+import { assert } from 'chai'
+
+describe('fake test', () => {
+  it('does nothing', () => {
+    assert.deepEqual({}, {})
+  })
+})
