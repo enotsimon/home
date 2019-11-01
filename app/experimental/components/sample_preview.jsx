c7ecb171d43155a3ff46a8177ed1028fd1eaae7e
enot simon <sy.enot@gmail.com>
2019-11-01 14:13:03 +0300

    little refactoring of samples collection

--- app/experimental/components/sample_preview.jsx
+++ app/experimental/components/sample_preview.jsx
@@ -1,46 +1,42 @@
-
+// @flow
 import React from 'react'
 
-export default class SamplePreview extends React.Component {
-  constructor(props) {
-    super()
-    const valid_statuses = ['draft', 'in_progress', 'almost_ready', 'ready']
+export type SamplePreviewStatus = 'draft' | 'in_progress' | 'almost_ready' | 'ready'
 
-    if (!props.name || !props.description || !props.sample_url || !props.img_path || !props.status) {
-      console.log('some params missing', props)
-      throw ('some params missing')
-    }
-    if (valid_statuses.indexOf(props.status) == -1) {
-      console.log('wrong status', props.status)
-      throw ('wrong status')
-    }
-    this.state = { status_text: props.status.replace(/_/g, ' ') }
-  }
+export type SamplePreviewConfig = {
+  name: string,
+  description: string,
+  sample_url: string,
+  img_path: string,
+  status: SamplePreviewStatus,
+}
 
-
-  render() {
-    return (
-      <div className="sample_preview">
-        <div className="panel panel-success" style={{ width: 220 }}>
-          <div className="panel-heading">
-            {/* class navbar-text? */}
-            <div className="panel-title">
-              {this.props.name}
-            </div>
-            <span className={`label label-primary status_${this.props.status}`}>
-              {this.state.status_text}
-            </span>
+const SamplePreview = (props: SamplePreviewConfig) => {
+  const statusText = props.status.replace(/_/g, ' ')
+  const statusClass = `status_${props.status}`
+  return (
+    <div className="sample_preview">
+      <div className="panel panel-success" style={{ width: 220 }}>
+        <div className="panel-heading">
+          {/* class navbar-text? */}
+          <div className="panel-title">
+            {props.name}
           </div>
-          <div className="panel-body">
-            <a href={this.props.sample_url} className="thumbnail">
-              <img width="200" height="200" src={this.props.img_path} />
-            </a>
-            <div className="">
-              <p>{this.props.description}</p>
-            </div>
+          <span className={`label label-primary ${statusClass}`}>
+            {statusText}
+          </span>
+        </div>
+        <div className="panel-body">
+          <a href={props.sample_url} className="thumbnail">
+            <img alt="sample preview" width="200" height="200" src={props.img_path} />
+          </a>
+          <div>
+            <p>{props.description}</p>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default SamplePreview
--- app/experimental/components/samples_collection.jsx
+++ app/experimental/components/samples_collection.jsx
@@ -1,6 +1,78 @@
-
+// @flow
 import SamplePreview from 'experimental/components/sample_preview'
 import React from 'react'
+
+import type { SamplePreviewConfig } from 'experimental/components/sample_preview'
+
+const config: Array<SamplePreviewConfig> = [
+  {
+    name: 'moving arrows',
+    description: `schizophreniac arrows moving all around.
+      add cosinus interpolation on arrow's turns`,
+    sample_url: './moving_arrows.html',
+    img_path: './thumbnails/moving_arrows.png',
+    status: 'almost_ready',
+  },
+  {
+    name: 'planets focus',
+    description: `star system emulation, where we
+      dynamically change focus on random stellar body,
+      i.e. move it to the center of coordinates and make all others spin around it`,
+    sample_url: './planets_focus.html',
+    img_path: './thumbnails/planets_focus.jpg',
+    status: 'in_progress',
+  },
+  {
+    name: 'basic random tableau',
+    description: 'random start configuration, pixels change their colors with edgy jump from white to black',
+    sample_url: './basic_tableau.html',
+    img_path: './thumbnails/basic_tableau.jpg',
+    status: 'ready',
+  },
+  {
+    name: 'smooth random tableau',
+    description: 'random start configuration, pixels change their colors smoothly -- black to white and backward',
+    sample_url: './smooth_tableau.html',
+    img_path: './thumbnails/smooth_tableau.jpg',
+    status: 'ready',
+  },
+  {
+    name: 'rule 30',
+    description: 'cellular automaton evolution of rule 30 introduced by stephen wolfram in 1983',
+    sample_url: './rule_30.html',
+    img_path: './thumbnails/rule_30.jpg',
+    status: 'ready',
+  },
+  {
+    name: 'vichniac vote',
+    description: `the color of cell depends on number of black cell in its moore neighborhood and previous
+      self color -- it is black if total numbler black cells is greater than 4`,
+    sample_url: './vichniac_vote.html',
+    img_path: './thumbnails/vichniac_vote.jpg',
+    status: 'ready',
+  },
+  {
+    name: 'orbits',
+    description: 'experiment with 3d polar functions',
+    sample_url: './orbits.html',
+    img_path: './thumbnails/orbits.jpg',
+    status: 'in_progress',
+  },
+  {
+    name: 'luna',
+    description: 'its like moon -- planet with craters on surface',
+    sample_url: './luna.html',
+    img_path: './thumbnails/luna.jpg',
+    status: 'ready',
+  },
+  {
+    name: 'wavy sphere',
+    description: 'sphere with waves',
+    sample_url: './wavy_sphere.html',
+    img_path: './thumbnails/wavy_planet.jpg',
+    status: 'ready',
+  },
+]
 
 export default class SamplesCollecton extends React.Component {
   render() {
@@ -13,83 +85,7 @@ export default class SamplesCollecton extends React.Component {
             </h4>
           </div>
           <div className="panel-body">
-
-            {React.createElement(SamplePreview, {
-              name: 'moving arrows',
-              description: `schizophreniac arrows moving all around.
-                add cosinus interpolation on arrow's turns`,
-              sample_url: './moving_arrows.html',
-              img_path: './thumbnails/moving_arrows.png',
-              status: 'almost_ready',
-            })}
-
-            {React.createElement(SamplePreview, {
-              name: 'planets focus',
-              description: `star system emulation, where we
-                dynamically change focus on random stellar body,
-                i.e. move it to the center of coordinates and make all others spin around it`,
-              sample_url: './planets_focus.html',
-              img_path: './thumbnails/planets_focus.jpg',
-              status: 'in_progress',
-            })}
-
-            {React.createElement(SamplePreview, {
-              name: 'basic random tableau',
-              description: 'random start configuration, pixels change their colors with edgy jump from white to black',
-              sample_url: './basic_tableau.html',
-              img_path: './thumbnails/basic_tableau.jpg',
-              status: 'ready',
-            })}
-
-            {React.createElement(SamplePreview, {
-              name: 'smooth random tableau',
-              description: 'random start configuration, pixels change their colors smoothly -- black to white and backward',
-              sample_url: './smooth_tableau.html',
-              img_path: './thumbnails/smooth_tableau.jpg',
-              status: 'ready',
-            })}
-
-            {React.createElement(SamplePreview, {
-              name: 'rule 30',
-              description: 'cellular automaton evolution of rule 30 introduced by stephen wolfram in 1983',
-              sample_url: './rule_30.html',
-              img_path: './thumbnails/rule_30.jpg',
-              status: 'ready',
-            })}
-
-            {React.createElement(SamplePreview, {
-              name: 'vichniac vote',
-              description: `the color of cell depends on number of black cell in its moore neighborhood and previous
-                self color -- it is black if total numbler black cells is greater than 4`,
-              sample_url: './vichniac_vote.html',
-              img_path: './thumbnails/vichniac_vote.jpg',
-              status: 'ready',
-            })}
-
-            {React.createElement(SamplePreview, {
-              name: 'orbits',
-              description: 'experiment with 3d polar functions',
-              sample_url: './orbits.html',
-              img_path: './thumbnails/orbits.jpg',
-              status: 'in_progress',
-            })}
-
-            {React.createElement(SamplePreview, {
-              name: 'luna',
-              description: 'its like moon -- planet with craters on surface',
-              sample_url: './luna.html',
-              img_path: './thumbnails/luna.jpg',
-              status: 'ready',
-            })}
-
-            {React.createElement(SamplePreview, {
-              name: 'wavy sphere',
-              description: 'sphere with waves',
-              sample_url: './wavy_sphere.html',
-              img_path: './thumbnails/wavy_planet.jpg',
-              status: 'ready',
-            })}
-
+            {config.map(e => React.createElement(SamplePreview, { ...e, key: e.name }))}
           </div>
         </div>
       </div>
