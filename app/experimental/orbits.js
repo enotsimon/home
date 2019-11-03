2915a553cf770405f1237601e95607bbaf6dc949
enot simon <sy.enot@gmail.com>
2019-11-03 13:40:28 +0300

    rewrite orbits

--- app/experimental/orbits.js
+++ app/experimental/orbits.js
@@ -1,69 +1,82 @@
-
+// @flow
 import Util from 'common/util'
 import Color from 'common/color'
-import BasicDrawer from 'experimental/basic_drawer'
 import * as PIXI from 'pixi.js'
 
-export default class Orbits extends BasicDrawer {
-  constructor() {
-    super('circle')
-  }
+import { createDrawer } from 'experimental/drawer'
+import type { DrawerState } from 'experimental/drawer'
 
-  update_debug_info() {
-    return [
-      { id: 'debug_info_precession', text: 'precession', value: this.figures ? this.figures[0].precession_coef : '' },
-      { id: 'debug_info_nutation', text: 'nutation', value: this.figures ? this.figures[0].nutation_coef : '' },
-      { id: 'debug_info_additional', text: 'angle this.acceleration', value: this.acceleration },
-    ]
-  }
+type Figure = {
+  id: number,
+  graphics: Object, // PIXI
+  radius: number,
+  // rotation_coef: .0025,
+  precession_coef: number,
+  nutation_coef: number,
+}
 
-  init_graphics() {
-    this.count_figures = 1
-    this.figures = [...Array(this.count_figures).keys()].map(i => {
-      const graphics = new PIXI.Graphics()
-      this.base_container.addChild(graphics)
-      return {
-        id: i,
-        graphics,
-        radius: 0.9 * 0.5 * this.size,
-        // rotation_coef: .0025,
-        precession_coef: 0.0025 * Util.rand_float(-3, 3),
-        nutation_coef: 0.0025 * Util.rand_float(1, 3),
-      }
-    })
-  }
+const updateDebugInfo = (state) => [
+  { id: 'debug_info_precession', text: 'precession', value: state.figures ? state.figures[0].precession_coef : '' },
+  { id: 'debug_info_nutation', text: 'nutation', value: state.figures ? state.figures[0].nutation_coef : '' },
+  { id: 'debug_info_additional', text: 'angle state.acceleration', value: state.acceleration },
+]
 
-  redraw() {
-    this.figures.forEach(figure => this.draw_full_circle(figure))
-  }
-
-  calc_single_point(radius, angle, precession, nutation) {
-    const x = radius * Math.cos(angle) * Math.sin(nutation)
-    const y = radius * Math.sin(angle)
-    const sp = Math.sin(precession)
-    const cp = Math.cos(precession)
-    const nx = x * cp - y * sp
-    const ny = x * sp + y * cp
-    return { x: nx, y: ny }
-  }
-
-  draw_full_circle(figure) {
-    figure.graphics.clear()
-    const count_dots = 2 * 36
-    this.acceleration = 5 * Math.cos(0.005 * this.tick_time)
-    for (let angle = 0; angle <= 2 * Math.PI; angle += 2 * Math.PI / count_dots) {
-      const coords = this.calc_single_point(
-        figure.radius,
-        // angle + (figure.rotation_coef * this.tick_time) + this.acceleration,
-        // i dont understand why its really this.acceleration and where is the speed?
-        angle + this.acceleration,
-        figure.precession_coef * this.tick_time,
-        figure.nutation_coef * this.tick_time + 0.5 * this.acceleration
-      )
-      figure.graphics.beginFill(Color.to_pixi([255, 255, 255]))
-      // figure.graphics.drawRect(coords.x, coords.y, .5, .5);
-      figure.graphics.drawCircle(coords.x, coords.y, 1)
-      figure.graphics.endFill()
+const initGraphics = (oldState: DrawerState): DrawerState => {
+  const state = { ...oldState }
+  state.count_figures = 1
+  state.figures = [...Array(state.count_figures).keys()].map(i => {
+    const graphics = new PIXI.Graphics()
+    state.base_container.addChild(graphics)
+    return {
+      id: i,
+      graphics,
+      radius: 0.9 * 0.5 * state.size,
+      // rotation_coef: .0025,
+      precession_coef: 0.0025 * Util.rand_float(-3, 3),
+      nutation_coef: 0.0025 * Util.rand_float(1, 3),
     }
+  })
+  return state
+}
+
+const redraw = (oldState: DrawerState): DrawerState => {
+  const state = { ...oldState }
+  state.acceleration = 5 * Math.cos(0.005 * state.tickTime)
+  state.figures.forEach(figure => drawFullCircle(figure, state))
+  return state
+}
+
+const drawFullCircle = (figure: Figure, state: DrawerState): void => {
+  figure.graphics.clear()
+  const count_dots = 2 * 36
+  for (let angle = 0; angle <= 2 * Math.PI; angle += 2 * Math.PI / count_dots) {
+    const coords = calcSinglePoint(
+      figure.radius,
+      // angle + (figure.rotation_coef * state.tickTime) + state.acceleration,
+      // i dont understand why its really state.acceleration and where is the speed?
+      angle + state.acceleration,
+      figure.precession_coef * state.tickTime,
+      figure.nutation_coef * state.tickTime + 0.5 * state.acceleration
+    )
+    figure.graphics.beginFill(Color.to_pixi([255, 255, 255]))
+    figure.graphics.drawCircle(coords.x, coords.y, 1)
+    figure.graphics.endFill()
   }
 }
+
+const calcSinglePoint = (radius, angle, precession, nutation) => {
+  const x = radius * Math.cos(angle) * Math.sin(nutation)
+  const y = radius * Math.sin(angle)
+  const sp = Math.sin(precession)
+  const cp = Math.cos(precession)
+  const nx = x * cp - y * sp
+  const ny = x * sp + y * cp
+  return { x: nx, y: ny }
+}
+
+createDrawer(
+  'circle',
+  updateDebugInfo,
+  initGraphics,
+  redraw,
+)
