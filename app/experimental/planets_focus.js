665cfc4e5ad8ae204c4a9d1941cf23577f6aaae9
enot simon <sy.enot@gmail.com>
2019-11-05 12:44:01 +0300

    rewrite planets_focus

--- app/experimental/planets_focus.js
+++ app/experimental/planets_focus.js
@@ -1,122 +1,139 @@
-
+// @flow
 import Util from 'common/util'
 import Color from 'common/color'
-import BasicDrawer from 'experimental/basic_drawer'
 import * as PIXI from 'pixi.js'
 import * as d3 from 'd3'
 
-/**
- *
- */
-export default class PlanetsFocus extends BasicDrawer {
-  constructor() {
-    super('circle')
-  }
+import { createDrawer } from 'experimental/drawer'
+import type { DrawerState } from 'experimental/drawer'
 
-  init_graphics() {
-    this.matrix = new PIXI.Container()
-    this.base_container.addChild(this.matrix)
-    this.bodies = []
-    this.init_bodies()
-    this.forced_focus = false
-    this.focused_body = this.bodies[0]
-    this.focus_change_threshold = 300
-    this.focus_change_tick = this.focus_change_threshold
-    this.update_matrix_by_focus()
-  }
-
-  redraw() {
-    this.bodies.forEach(body => {
-      body.orbital_angle += this.tick_delta * body.orbital_speed
-      body.angle += this.tick_delta * (body.orbital_speed + body.rotation_speed)
-      this.set_graphics_transform_by_stellar_coords(body)
-    })
-    if (--this.focus_change_tick <= 0) {
-      this.focus_change_tick = this.focus_change_threshold
-      if (!this.forced_focus) {
-        const cur_index = this.bodies.indexOf(this.focused_body)
-        this.focused_body = this.bodies[cur_index == this.bodies.length - 1 ? 0 : cur_index + 1]
-        console.log('now focus on', this.focused_body.name)
-      }
-    }
-    this.update_matrix_by_focus()
-    d3.select('#debug_info_focus_on').html(this.focused_body.name)
-  }
-
-
-  update_matrix_by_focus() {
-    // TODO do it second time on each frame, bad
-    const coords = this.calc_coords_recursively(this.focused_body)
-    this.matrix.position.x = -coords.x
-    this.matrix.position.y = -coords.y
-  }
-
-  calc_coords_recursively(body, acc = { x: 0, y: 0 }) {
-    const coords = Util.from_polar_coords(body.orbital_angle, body.orbital_radius)
-    acc.x += coords.x
-    acc.y += coords.y
-    if (body.parent) {
-      return this.calc_coords_recursively(body.parent, acc)
-    }
-    return acc
-  }
-
-
-  init_bodies() {
-    // name, parent, orbital_radius, radius, orbital_speed, rotation_speed, [orbital_angle], [angle]
-    this.star = this.init_body('star', null, 0, 10, 0, 0.5)
-    this.planet1 = this.init_body('planet1', this.star, 20, 3, 1, 0)
-    this.moon1 = this.init_body('moon1', this.planet1, 6, 1, 2, 2)
-    this.planet2 = this.init_body('planet2', this.star, 40, 5, 2, 3)
-    this.moon21 = this.init_body('moon21', this.planet2, 8, 2, 3, 0.1)
-    this.moon22 = this.init_body('moon22', this.planet2, 12, 1, 4, 1)
-  }
-
-  init_body(name, parent, orbital_radius, radius, orbital_speed, rotation_speed) {
-    const body = new StellarBody(name, parent, orbital_radius, radius, orbital_speed, rotation_speed)
-    this.init_body_graphics(body, parent)
-    const parent_graphics = parent ? parent.base_container : this.matrix
-    parent_graphics.addChild(body.base_container)
-    this.bodies.push(body)
-    return body
-  }
-
-  init_body_graphics(body, parent) {
-    // thats because base_container do not rotate with planet rotation, otherwise
-    // all moons rotate with planet's self rotations
-    body.base_container = new PIXI.Graphics()
-    body.graphics = new PIXI.Graphics()
-    body.base_container.addChild(body.graphics)
-    this.set_graphics_transform_by_stellar_coords(body)
-    body.graphics.lineStyle(body.radius / 10, Color.to_pixi([255, 255, 255]))
-    body.graphics.drawCircle(0, 0, body.radius)
-    body.graphics.closePath();
-    ([1, 2, 3]).forEach(i => {
-      const coords = Util.from_polar_coords(i * 2 * Math.PI / 3, body.radius / 2)
-      body.graphics.drawCircle(coords.x, coords.y, body.radius / 4)
-      body.graphics.closePath()
-    })
-  }
-
-  set_graphics_transform_by_stellar_coords(body) {
-    const coords = Util.from_polar_coords(body.orbital_angle, body.orbital_radius)
-    // thats not a mistake, see comment below
-    body.base_container.x = coords.x
-    body.base_container.y = coords.y
-    body.graphics.rotation = body.angle
-  }
+type StellarBody = {
+  base_container: ?Object,
+  graphics: ?Object,
+  name: string,
+  parent: ?StellarBody,
+  orbital_radius: number,
+  radius: number,
+  orbital_speed: number,
+  rotation_speed: number,
+  orbital_angle: number,
+  angle: number,
 }
 
-class StellarBody {
-  constructor(name, parent, orbital_radius, radius, orbital_speed, rotation_speed, orbital_angle = false, angle = false) {
-    const radius_factor = 1.5 // DEBUG!!!
-    this.name = name
-    this.parent = parent
-    this.orbital_radius = radius_factor * orbital_radius
-    this.radius = radius_factor * radius
-    this.orbital_speed = Util.radians(orbital_speed)
-    this.rotation_speed = Util.radians(rotation_speed)
-    this.orbital_angle = orbital_angle || 2 * Math.PI * Math.random()
-    this.angle = angle || 2 * Math.PI * Math.random()
-  }
+const initGraphics = (oldState: DrawerState): DrawerState => {
+  let state = { ...oldState }
+  state.matrix = new PIXI.Container()
+  state.base_container.addChild(state.matrix)
+  // init bodies
+  state.star = initBody(state, 'star', null, 0, 10, 0, 0.5)
+  state.planet1 = initBody(state, 'planet1', state.star, 20, 3, 1, 0)
+  state.moon1 = initBody(state, 'moon1', state.planet1, 6, 1, 2, 2)
+  state.planet2 = initBody(state, 'planet2', state.star, 40, 5, 2, 3)
+  state.moon21 = initBody(state, 'moon21', state.planet2, 8, 2, 3, 0.1)
+  state.moon22 = initBody(state, 'moon22', state.planet2, 12, 1, 4, 1)
+  state.bodies = [state.star, state.planet1, state.moon1, state.planet2, state.moon21, state.moon22]
+  state.forced_focus = false
+  state.focused_body = state.bodies[0]
+  state.focus_change_threshold = 300
+  state.focus_change_tick = state.focus_change_threshold
+  state = updateMatrixByFocus(state)
+  return state
 }
+
+const redraw = (oldState: DrawerState): DrawerState => {
+  let state = { ...oldState }
+  state.bodies = state.bodies.map(origBody => {
+    const body = { ...origBody }
+    body.orbital_angle += state.tick_delta * body.orbital_speed
+    body.angle += state.tick_delta * (body.orbital_speed + body.rotation_speed)
+    return setGraphicsTransformByStellarCoords(body)
+  })
+  state.focus_change_tick -= 1
+  if (state.focus_change_tick <= 0) {
+    state.focus_change_tick = state.focus_change_threshold
+    if (!state.forced_focus) {
+      const cur_index = state.bodies.indexOf(state.focused_body)
+      state.focused_body = state.bodies[cur_index === state.bodies.length - 1 ? 0 : cur_index + 1]
+      console.log('now focus on', state.focused_body.name)
+    }
+  }
+  state = updateMatrixByFocus(state)
+  d3.select('#debug_info_focus_on').html(state.focused_body.name)
+  return state
+}
+
+const updateMatrixByFocus = (oldState: DrawerState): DrawerState => {
+  const state = { ...oldState }
+  // TODO do it second time on each frame, bad
+  const coords = calc_coords_recursively(state.focused_body)
+  state.matrix.position.x = -coords.x
+  state.matrix.position.y = -coords.y
+  return state
+}
+
+const calc_coords_recursively = (body, acc = { x: 0, y: 0 }) => {
+  const coords = Util.from_polar_coords(body.orbital_angle, body.orbital_radius)
+  acc.x += coords.x
+  acc.y += coords.y
+  if (body.parent) {
+    return calc_coords_recursively(body.parent, acc)
+  }
+  return acc
+}
+
+const initBody = (state, name, parent, orbital_radius, radius, orbital_speed, rotation_speed): StellarBody => {
+  const radius_factor = 1.5 // DEBUG!!!
+  const orbital_angle = false
+  const angle = false
+  const BodyRadius = radius_factor * radius
+
+  //
+  // thats because base_container do not rotate with planet rotation, otherwise
+  // all moons rotate with planet's self rotations
+  const base_container = new PIXI.Graphics()
+  const graphics = new PIXI.Graphics()
+  base_container.addChild(graphics)
+  graphics.lineStyle(BodyRadius / 10, Color.to_pixi([255, 255, 255]))
+  graphics.drawCircle(0, 0, BodyRadius)
+  graphics.closePath();
+  ([1, 2, 3]).forEach(i => {
+    const coords = Util.from_polar_coords(i * 2 * Math.PI / 3, BodyRadius / 2)
+    graphics.drawCircle(coords.x, coords.y, BodyRadius / 4)
+    graphics.closePath()
+  })
+  //
+
+  let body = {
+    base_container,
+    graphics,
+    name,
+    parent,
+    orbital_radius: radius_factor * orbital_radius,
+    radius: BodyRadius,
+    orbital_speed: Util.radians(orbital_speed),
+    rotation_speed: Util.radians(rotation_speed),
+    orbital_angle: orbital_angle || 2 * Math.PI * Math.random(),
+    angle: angle || 2 * Math.PI * Math.random(),
+  }
+  body = setGraphicsTransformByStellarCoords(body)
+  const parent_graphics = parent ? parent.base_container : state.matrix
+  parent_graphics.addChild(body.base_container)
+  return body
+}
+
+const setGraphicsTransformByStellarCoords = (origBody: StellarBody): StellarBody => {
+  const body = { ...origBody }
+  const coords = Util.from_polar_coords(body.orbital_angle, body.orbital_radius)
+  // thats not a mistake, see comment below
+  body.base_container.x = coords.x
+  body.base_container.y = coords.y
+  body.graphics.rotation = body.angle
+  return body
+}
+
+createDrawer(
+  'circle',
+  () => [],
+  initGraphics,
+  redraw,
+)
