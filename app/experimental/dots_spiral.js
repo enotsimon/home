76a2b90ceeb8fa980b5ae92f8c84f41df25c5c04
enot simon <sy.enot@gmail.com>
2019-11-07 14:48:51 +0300

    better dots_spiral random dots

--- app/experimental/dots_spiral.js
+++ app/experimental/dots_spiral.js
@@ -15,11 +15,16 @@ type DotsState = DrawerState & {
   dotsGraphics: Array<Object>, // PIXI graphics
 }
 
+type DotId = number
 type Dot = {
+  id: DotId,
   angle: number,
   radius: number,
-  parent: ?Dot,
-  children: Array<Dot>,
+  // store them cause too ofter we need to convert from polar
+  x: number,
+  y: number,
+  parent: ?DotId,
+  children: Array<DotId>,
 }
 
 const initGraphics = (oldState: DrawerState): DotsState => {
@@ -27,21 +32,16 @@ const initGraphics = (oldState: DrawerState): DotsState => {
   const totalDots = 100
   const seed = Date.now()
   random.use(seedrandom(seed))
-  state.dots = R.range(1, totalDots).map(() => ({
-    angle: random.float(0, 2 * Math.PI),
-    radius: random.float(),
-    parent: null,
-    children: [],
-  }))
+  // TODO min distance between
+  state.dots = recursiveAddDots(state.size / 2, totalDots)
   // Color.to_pixi([255, 255, 255])
   state.dotsGraphics = state.dots.map(dot => {
-    const { x, y } = Util.from_polar_coords(dot.angle, dot.radius)
     const graphics = new PIXI.Graphics()
     graphics.beginFill(Color.to_pixi([255, 255, 255]), 1)
     graphics.drawCircle(0, 0, 1)
     graphics.endFill()
-    graphics.x = state.size / 2 * x
-    graphics.y = state.size / 2 * y
+    graphics.x = dot.x
+    graphics.y = dot.y
     state.base_container.addChild(graphics)
     return graphics
   })
@@ -51,6 +51,27 @@ const initGraphics = (oldState: DrawerState): DotsState => {
 
 const redraw = (state: DrawerState): DrawerState => {
   return state
+}
+
+const recursiveAddDots = (scale: number, limit: number, dots = [], cycles: number = 0) => {
+  const theVeryDistanceLimit = 0.02 * scale
+  if (limit === 0) {
+    return dots
+  }
+  if (cycles === 1000) {
+    throw new Error('too many cycles')
+  }
+  // FIXME не равномерное распределение!
+  const angle = random.float(0, 2 * Math.PI)
+  const radius = scale * random.float()
+  const { x, y } = Util.from_polar_coords(angle, radius)
+  const tooClose = R.find(d => Util.distance(d, { x, y }) <= theVeryDistanceLimit)(dots)
+  if (tooClose) {
+    return recursiveAddDots(scale, limit, dots, cycles + 1)
+  }
+  const dot = { angle, radius, x, y, parent: null, children: [] }
+  console.log('i add', dot)
+  return recursiveAddDots(scale, limit - 1, [...dots, dot], 0)
 }
 
 export const initDotsSpiral = () => initDrawer(
