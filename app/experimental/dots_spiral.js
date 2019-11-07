b6e2a13dc79f00c993a30356f6cc004fe50e9e44
enot simon <sy.enot@gmail.com>
2019-11-07 18:18:18 +0300

    dots_spiral add building and drawing spiral, but wrong way for now

--- app/experimental/dots_spiral.js
+++ app/experimental/dots_spiral.js
@@ -3,6 +3,7 @@ import Util from 'common/util'
 import Color from 'common/color'
 import * as PIXI from 'pixi.js'
 import * as R from 'ramda'
+import * as U from 'common/utils'
 import random from 'random'
 import seedrandom from 'seedrandom'
 
@@ -35,7 +36,6 @@ const initGraphics = (oldState: DrawerState): DotsState => {
   // TODO min distance between
   state.dots = recursiveAddDots(state.size / 2, totalDots)
   state.dots = connectDotsToSpiral(state.dots)
-  // Color.to_pixi([255, 255, 255])
   state.dotsGraphics = state.dots.map(dot => {
     const graphics = new PIXI.Graphics()
     graphics.beginFill(Color.to_pixi([255, 255, 255]), 1)
@@ -46,6 +46,8 @@ const initGraphics = (oldState: DrawerState): DotsState => {
     state.base_container.addChild(graphics)
     return graphics
   })
+  // TODO add this crap to state
+  drawLines(state.dots, state.base_container)
 
   return state
 }
@@ -92,14 +94,51 @@ const connectDotsToSpiral = (dots: Array<Dot>): Array<Dot> => {
   return connectDotsToSpiralRecursive(mostDistantDot, dots)
 }
 
-const connectDotsToSpiralRecursive = (curDot: Dot, origDots: Array<Dot>): Array<Dot> => {
-  const dots = [...origDots]
-  const openList = R.filter(d => d.id !== curDot.id && R.length(d.children) === 0)
+const connectDotsToSpiralRecursive = (curDot: Dot, origDots: Array<Dot>, cycles = 0): Array<Dot> => {
+  if (cycles > origDots.length) {
+    throw new Error('eternal cycle!')
+  }
+  let dots = [...origDots]
+  const openList = R.filter(d => d.id !== curDot.id && R.length(d.children) === 0)(dots)
   if (R.length(openList) === 0) {
     return dots
   }
-  // TODO
-  return dots
+  const anyDamnPoint = { x: 2 * curDot.x, y: curDot.y }
+  const openListWithAngles = openList.map(d => ({ ...d, diffAngle: U.angleBy3Points(anyDamnPoint, curDot, d) }))
+  const sortedByAngle = R.sort((d1, d2) => d2.diffAngle - d1.diffAngle)(openListWithAngles)
+  const nextDot = sortedByAngle[0]
+  const newNextDot = { ...nextDot, parent: curDot.id }
+  const newCurDot = { ...curDot, children: [nextDot.id] }
+  // fuck!
+  dots = dots.map(d => {
+    if (d.id === newCurDot.id) {
+      return newCurDot
+    }
+    if (d.id === newNextDot.id) {
+      return newNextDot
+    }
+    return d
+  })
+  return connectDotsToSpiralRecursive(newNextDot, dots, cycles + 1)
+}
+
+const drawLines = (dots: Array<Dot>, container: Object): void => {
+  dots.forEach(dot => {
+    if (R.length(dot.children) === 0) {
+      return
+    }
+    dot.children.forEach(idTo => {
+      const target = R.find(d => d.id === idTo)(dots)
+      if (!target) {
+        throw new Error('crap is no the way, i cant find dot by id')
+      }
+      const graphics = new PIXI.Graphics()
+      graphics.lineStyle(1, Color.to_pixi([255, 255, 255]))
+      graphics.moveTo(dot.x, dot.y)
+      graphics.lineTo(target.x, target.y)
+      container.addChild(graphics)
+    })
+  })
 }
 
 export const initDotsSpiral = () => initDrawer(
