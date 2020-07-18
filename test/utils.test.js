9ffd3c2e90c994881b22c364fc6dbc66cde45ab0
Иван Скоробогатько <skorobogatko@superjob.ru>
2020-07-18 11:28:27 +0300

    utils add pairs() and shuffle()

--- test/utils.test.js
+++ test/utils.test.js
@@ -157,3 +157,9 @@ describe('intervalsCrossPoint', () => {
     })
   })
 })
+
+describe('pairs', () => {
+  it('should return all pairs for 1, 2, 3', () => {
+    assert.deepEqual(U.pairs([1, 2, 3]), [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]])
+  })
+})
