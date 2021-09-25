f57c346d69097de0c08cec5031936fff8de444a9
enot simon <sy.enot@gmail.com>
2021-09-25 16:51:46 +0300

    rrt_basic.js changes in drawings

--- app/experimental/samples/rrt_basic.js
+++ app/experimental/samples/rrt_basic.js
@@ -18,7 +18,8 @@ const STEP = 5
 type State = {|
   ...DrawerState,
   rrt: RRTDiagram,
-  graphics: Object,
+  pointsGraphics: Object,
+  linksGraphics: Object,
   curIndex: number,
 |}
 
@@ -26,13 +27,17 @@ const initGraphics = (state: State): State => {
   const seed = Date.now()
   random.use(seedrandom(seed))
   state.base_container.removeChildren()
-  const graphics = new PIXI.Graphics()
-  state.base_container.addChild(graphics)
-  addCircleMask(graphics, state.size / 2, { x: 0, y: 0 }, [100, 0, 0])
+  // order emulates z-index!
+  const linksGraphics = new PIXI.Graphics()
+  state.base_container.addChild(linksGraphics)
+  const pointsGraphics = new PIXI.Graphics()
+  state.base_container.addChild(pointsGraphics)
+  addCircleMask(state.base_container, state.size / 2, { x: 0, y: 0 }, [100, 0, 0])
   const rrt = generate(STEP, randomPointFunc(state.size / 2), { x: 0, y: state.size / 2 })
   return {
     ...state,
-    graphics,
+    pointsGraphics,
+    linksGraphics,
     rrt,
     curIndex: 1, // 1 because we dont draw root point! be careful! _we dont draw root point_!!!
   }
@@ -51,18 +56,25 @@ const redraw = (state: State): State => {
     return { ...state, curIndex: state.curIndex + 1 }
   }
   const parent = state.rrt[point.parent]
-  drawCircleAndLine(state.graphics, point, parent)
+  const mainColor = Math.max(50, 255 - (point.index / 3))
+  const radius = Math.max(0.5, 2 - (point.index / 250))
+  const bgColor = Math.max(10, mainColor / 2)
+  drawPoint(state.pointsGraphics, point, mainColor, bgColor, radius)
+  drawLink(state.linksGraphics, point, parent, bgColor, radius)
   return { ...state, curIndex: state.curIndex + 1 }
 }
 
-const drawCircleAndLine = (graphics: Object, point: RRTPoint, parent: RRTPoint): void => {
-  const pointColor = Math.max(50, 255 - (point.index / 3))
-  const lineColor = Math.max(10, pointColor / 2)
-  const lineWidth = Math.max(0.5, 2 - (point.index / 250))
-  graphics.beginFill(Color.to_pixi([pointColor, 0, 0]), 1)
-  graphics.drawCircle(point.x, point.y, 0.5)
+const drawPoint = (graphics: Object, point: RRTPoint, mainColor, bgColor, radius): void => {
+  graphics.beginFill(Color.to_pixi([bgColor, 0, 0]), 1)
+  graphics.drawCircle(point.x, point.y, radius / 1.25)
   graphics.endFill()
-  graphics.lineStyle(lineWidth, Color.to_pixi([lineColor, 0, 0]), 0.5)
+  graphics.beginFill(Color.to_pixi([mainColor, 0, 0]), 1)
+  graphics.drawCircle(point.x, point.y, radius / 3)
+  graphics.endFill()
+}
+
+const drawLink = (graphics: Object, point: RRTPoint, parent: RRTPoint, bgColor, radius): void => {
+  graphics.lineStyle(radius, Color.to_pixi([bgColor, 0, 0]), 0.5)
   graphics.moveTo(parent.x, parent.y)
   graphics.lineTo(point.x, point.y)
 }
