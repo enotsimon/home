9cc72ecda5d38e9276a0aab29d147d5ffd5fbfca
enot simon <sy.enot@gmail.com>
2022-05-03 15:06:29 +0300

    rrt_map return to very basics

--- app/experimental/samples/rrt_map.js
+++ app/experimental/samples/rrt_map.js
@@ -6,11 +6,11 @@
 import * as PIXI from 'pixi.js'
 import random from 'random'
 import seedrandom from 'seedrandom'
-import * as R from 'ramda'
+// import * as R from 'ramda'
 
 import * as Color from 'common/color'
 import * as U from 'common/utils'
-import { addCircleMask } from 'experimental/drawing_functions'
+// import { addCircleMask } from 'experimental/drawing_functions'
 import { startDrawer } from 'experimental/drawer'
 import { generate } from 'common/rrt_diagram'
 import { randomPointPolar } from 'experimental/random_points'
@@ -20,7 +20,7 @@ import type { RRTDiagram, RRTPoint } from 'common/rrt_diagram'
 
 const STEP = 5
 const COLOR_MAX = [0, 255, 0]
-const COLOR_MIN = [0, 25, 0]
+// const COLOR_MIN = [0, 25, 0]
 
 type State = {|
   ...DrawerState,
@@ -34,7 +34,7 @@ const initGraphics = (state: State): State => {
   random.use(seedrandom(seed))
   state.base_container.removeChildren()
   // order emulates z-index!
-  addCircleMask(state.base_container, state.size / 2, { x: 0, y: 0 }, COLOR_MIN)
+  // addCircleMask(state.base_container, state.size / 2, { x: 0, y: 0 }, COLOR_MIN)
   const linksGraphics = new PIXI.Graphics()
   state.base_container.addChild(linksGraphics)
   const pointsGraphics = new PIXI.Graphics()
@@ -51,17 +51,13 @@ const initGraphics = (state: State): State => {
 const randomPointFunc = (radius: number) => () => U.fromPolarCoords(randomPointPolar(radius))
 
 const drawRRT = ({ rrt, pointsGraphics, linksGraphics }: State): void => {
-  const generationMax = R.reduce((acc, { generation }) => (acc > generation ? acc : generation), 0, rrt)
-  const [rs, gs, bs] = R.map(i => (COLOR_MAX[i] - COLOR_MIN[i]) / generationMax, [0, 1, 2])
-  const radiusStep = 2 / generationMax
+  // const generationMax = R.reduce((acc, { generation }) => (acc > generation ? acc : generation), 0, rrt)
   rrt.forEach(point => {
-    if (point.parent !== null) {
+    if (point.parent !== null && point.parent !== undefined) {
       // console.log(point)
-      drawLink(linksGraphics, point, rrt[point.parent], [rs, gs, bs], COLOR_MAX, 1, radiusStep)
-    } else {
-      console.log('no parent', point)
+      drawLink(linksGraphics, point, rrt[point.parent], COLOR_MAX, 1)
     }
-    drawPoint(pointsGraphics, point, [rs, gs, bs], COLOR_MAX, [0, 0, 0], 1.5, radiusStep)
+    drawPoint(pointsGraphics, point, COLOR_MAX, [0, 0, 0], 1)
   })
 }
 
@@ -69,22 +65,17 @@ const redraw = (state: State): State => {
   return state
 }
 
-// minus cause generations go from root to leafs
-const calcColor = (mul, [rs, gs, bs], [rc, gc, bc]) => [rc - mul * rs, gc - mul * gs, bc - mul * bs]
-
-const drawPoint = (graphics: Object, point: RRTPoint, [rs, gs, bs], [rc, gc, bc], bgColor, radius, rStep): void => {
-  const color = calcColor(point.generation, [rs, gs, bs], [rc, gc, bc])
+const drawPoint = (graphics: Object, point: RRTPoint, color, bgColor, radius): void => {
   graphics.beginFill(Color.to_pixi(color), 1)
-  graphics.drawCircle(point.x, point.y, radius - point.generation * rStep)
+  graphics.drawCircle(point.x, point.y, radius)
   graphics.endFill()
   graphics.beginFill(Color.to_pixi(bgColor), 1)
-  graphics.drawCircle(point.x, point.y, (radius - point.generation * rStep) / 2)
+  graphics.drawCircle(point.x, point.y, radius / 2)
   graphics.endFill()
 }
 
-const drawLink = (graphics, point, parent: RRTPoint, [rs, gs, bs], [rc, gc, bc], width, wStep): void => {
-  const color = calcColor(point.generation, [rs, gs, bs], [rc, gc, bc])
-  graphics.lineStyle(width - point.generation * wStep, Color.to_pixi(color), 1)
+const drawLink = (graphics, point, parent: RRTPoint, color, width): void => {
+  graphics.lineStyle(width, Color.to_pixi(color), 1)
   graphics.moveTo(parent.x, parent.y)
   graphics.lineTo(point.x, point.y)
 }
