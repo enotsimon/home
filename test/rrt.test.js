70d09eb259bf08cdf8799095e514caa7f41ee2f2
enot simon <sy.enot@gmail.com>
2022-05-10 18:40:23 +0300

    fix test/rrt.test.js

--- test/rrt.test.js
+++ test/rrt.test.js
@@ -8,11 +8,11 @@ import * as RRT from '../app/common/rrt_diagram'
 describe('pointsByGenerations', () => {
   it('base case', () => {
     const rrt = [
-      { x: 0, y: 0, generation: 0, index: 0, parent: null },
-      { x: 1, y: 1, generation: 1, index: 1, parent: 0 },
-      { x: 0, y: 1, generation: 1, index: 2, parent: 0 },
-      { x: 1, y: 2, generation: 2, index: 3, parent: 1 },
-      { x: 1, y: 3, generation: 3, index: 4, parent: 2 },
+      { x: 0, y: 0, generation: 0, index: 0, parent: null, children: [1, 2] },
+      { x: 1, y: 1, generation: 1, index: 1, parent: 0, children: [3] },
+      { x: 0, y: 1, generation: 1, index: 2, parent: 0, children: [4] },
+      { x: 1, y: 2, generation: 2, index: 3, parent: 1, children: [] },
+      { x: 1, y: 3, generation: 3, index: 4, parent: 2, children: [] },
     ]
     const expect = [
       [rrt[0].index],
